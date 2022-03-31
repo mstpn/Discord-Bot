8f3229721c0cf89617a768e814c0ccb56fcd4212
Dru <awalsh0829@gmail.com>
2022-03-31 14:51:17 -0600

    Creation of user class blueprint

---
+++ user.py
@@ -0,0 +1,29 @@
+#Class User
+
+class User():
+
+    #==========================================================================
+    def __init__(self):
+        pass
+
+    def discordID():
+        pass
+
+    def firstName():
+        pass
+
+    def faculty():
+        pass
+
+    def programYear():
+        pass
+
+    def userName():
+        pass
+
+    def myEvents():
+        pass
+    #==========================================================================
+
+
+
