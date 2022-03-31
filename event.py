bd7308c80c4f2ba4e815b7acbf64793f4c287dbb
Dru <awalsh0829@gmail.com>
2022-03-31 17:33:54 -0600

    Added function declarations & variables for Event

--- event.py
+++ event.py
@@ -4,3 +4,24 @@
 
 import discord
 import asyncio
+
+class Event():
+    __creator = None
+    __duration = None
+    __startTime = None
+    __voice = None
+    __text = None
+
+    def __init__(self):
+        pass
+
+    def createVoiceChannel(__startTime, __duration):
+        __startTime = __startTime
+        __duration = __duration
+        pass
+
+    def createTextChannel(__startTime, __duration):
+        __startTime = __startTime
+        __duration = __duration
+        pass
+    
