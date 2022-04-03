765751928915c1fac63738444309cad57f4d672a
Dru <awalsh0829@gmail.com>
2022-04-03 15:00:51 -0600

    Adjusted messages.py to be a Class of Messages()

--- messages.py
+++ messages.py
@@ -1,45 +1,98 @@
+# messages.py
+
+# ==== Externally Built Main Imports ====
 import discord
 
-
+# ==== Externally Built Object Imports ====
 from random import randrange
 
-
+# ==== Locally Built Object Imports ====
 from games import games
+from commandsList import commandsList
 
-async def messagesHandler(message, client):
-    # Ensure it doesn't respond to itself
-    if message.author == client.user:
-        return
+# Messages Class
+# Purpose: To determine what message the user has sent and call the appropriate
+#          Bot Object method to handle the given input from a user.
+#
+# Variables: 
+#               - __botClient (private): Is the primary bot client connection to 
+#                                        the discord API
+#
+#               - __userMessage (private): The message recieved from a user within
+#                                          the chat system on discord
 
-    # ==================================================
-    #                 Status response
-    # ==================================================
-    elif message.content.lower().startswith('!idle'):
-        await message.channel.send(f"Ok {message.author.name}, I'm AFK!")
-        await client.change_presence(status=discord.Status.idle)
+class Messages():
 
-    elif message.content.lower().startswith('!dnd'):
-        await message.channel.send(f"Shhhh {message.author.name}, I need quiet!")
-        await client.change_presence(status=discord.Status.dnd)
+    #===================================================
+    #=                    Variables                    =
+    #===================================================
 
-    elif message.content.lower().startswith('!invis'):
-        await message.channel.send(f"Can't see me {message.author.name}!")
-        await client.change_presence(status=discord.Status.invisible)
+    __botClient = None
+    __userMessage = None
 
-    elif message.content.lower().startswith('!online'):
-        await message.channel.send(f"Alright {message.author.name}, I'm back!")
-        await client.change_presence(status=discord.Status.online)
 
-    # ==================================================
-    #                 Hello response
-    # ==================================================
-    elif message.content.lower().startswith('!hello'):
-        await message.channel.send(f"Hello {message.author.name}!")
-        
-    # ==================================================
-    #                 Game response
-    # ==================================================
-    elif message.content.lower().startswith('!game'):
-        game = discord.Game(games[randrange(len(games))])
-        await client.change_presence(activity=game)
-        await message.channel.send(f"Alright {message.author.name}, I'll play {game}")
+    #==========================================================
+    #=                    Public Functions                    =
+    #==========================================================
+
+    # -----------------------------
+    # ---- Default Constructor ----
+    # -----------------------------
+    def __init__(self):
+        pass
+
+    # --------------------------------
+    # ---- Overloaded Constructor ----
+    # --------------------------------
+    def __init__(self, message, client):
+        __botClient = client
+        __userMessage = message
+
+    # -----------------------------------------
+    # ---- Primary Message Command Handler ----
+    # -----------------------------------------
+    async def messagesHandler(self, userMessage, botClient):
+       
+        # ==== Ensure it doesn't respond to itself ====
+        if userMessage.author == botClient.user:
+            return
+        # ==== End Self Message Check ==== 
+
+
+        # ==== Hello Response ====
+        elif userMessage.content.lower().startswith(commandsList[0]):
+            await userMessage.channel.send(f"Hello {userMessage.author.name}!")
+            return
+        # ==== End Hello Response ====
+
+
+        # ==== Status Response(s) ====
+        elif userMessage.content.lower().startswith(commandsList[1]):
+            await userMessage.channel.send(f"Alright {userMessage.author.name}, I'm back!")
+            await botClient.change_presence(status=discord.Status.online)
+            return
+
+        elif userMessage.content.lower().startswith(commandsList[2]):
+            await userMessage.channel.send(f"Ok {userMessage.author.name}, I'm AFK!")
+            await botClient.change_presence(status=discord.Status.idle)
+            return
+
+        elif userMessage.content.lower().startswith(commandsList[3]):
+            await userMessage.channel.send(f"Shhhh {userMessage.author.name}, I need quiet!")
+            await botClient.change_presence(status=discord.Status.dnd)
+            return
+
+        elif userMessage.content.lower().startswith(commandsList[4]):
+            await userMessage.channel.send(f"Can't see me {userMessage.author.name}!")
+            await botClient.change_presence(status=discord.Status.invisible)
+            return
+        # ==== End Status Response(s) ====
+
+
+        # ==== Game Response ====
+        elif userMessage.content.lower().startswith(commandsList[5]):
+            game = discord.Game(games[randrange(len(games))])
+            await botClient.change_presence(activity=game)
+            await userMessage.channel.send(f"Alright {userMessage.author.name}, I'll play {game}")
+            return
+        # ==== End Game Response ====
