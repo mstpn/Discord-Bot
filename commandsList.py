8a11925db91684c4ada8f48c48343d63a17d6edf
Dru <awalsh0829@gmail.com>
2022-04-03 15:00:11 -0600

    Created a sperate file for commands (Need?)

---
+++ commandsList.py
@@ -0,0 +1,18 @@
+
+# List of Available Bot Commands
+# Could in the future have these on a local txt file
+commandsList = [
+    "!hello",   #0
+    "!online",  #1
+    "!idle",    #2
+    "!dnd",     #3
+    "!invis",   #4
+    "!game",    #5
+    "",         #6
+    "",         #7
+    "",         #8
+    "",         #9
+    "",         #10
+    "",         #11
+    "",         #12
+]
